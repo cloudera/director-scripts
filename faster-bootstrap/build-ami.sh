ef7666b091fc95e6e8924a053e2bf4c8abc8a5d4
Bill Havanki <bhavanki@cloudera.com>
2017-01-31 13:43:56 -0500

    Update examples to C5.10
    The following items are updated to use C5.10 as examples:
    
        sample configurations
        the faster-bootstrap build-ami.sh script
        examples shown in the UI for CM and CDH repository URLs
    
    Also, the supported OS lists in reference configs were updated to
    include CentOS/RHEL 6.8/7.3 where appropriate.

--- faster-bootstrap/build-ami.sh
+++ faster-bootstrap/build-ami.sh
@@ -42,9 +42,9 @@ Usage: $0 [options] <aws-region> <os> [<name>] [<parcel-url>] [<repository-url>]
   [<name>]:      An optional descriptive name for the new AMI.
       Default is calculated dynamically (specified by "AUTO")
   [<parcel-url>]:      Optional parcel URL to use for preloading.
-      Default http://archive.cloudera.com/cdh5/parcels/5.9/
+      Default http://archive.cloudera.com/cdh5/parcels/5.10/
   [<repository-url>]:  Optional Cloudera Manager yum repo to use for preloading.
-      Default http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.9/
+      Default http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.10/
 
 Be sure to specify <repository-url> for operating systems other than RHEL 7 or
 CentOS 7.
@@ -180,8 +180,8 @@ fi
 AWS_REGION=$1
 OS=$2
 NAME=${3-AUTO}
-CDH_URL=${4-"http://archive.cloudera.com/cdh5/parcels/5.9/"}
-CM_REPO_URL=${5-"http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.9/"}
+CDH_URL=${4-"http://archive.cloudera.com/cdh5/parcels/5.10/"}
+CM_REPO_URL=${5-"http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.10/"}
 
 # Validate OS TBD
 

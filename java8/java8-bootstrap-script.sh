c10eeda2099f75d600295d49be84a66f8886d336
Michael Wilson <mike.wilson@cloudera.com>
2017-07-12 21:30:34 -0400

    Update for Cloudera Director 2.5.0 release

--- java8/java8-bootstrap-script.sh
+++ java8/java8-bootstrap-script.sh
@@ -16,4 +16,4 @@
 
 # We remove any natively installed JDKs, as both Cloudera Manager and Cloudera Director only support Oracle JDKs
 yum remove --assumeyes *openjdk*
-rpm -ivh "http://archive.cloudera.com/director/redhat/7/x86_64/director/2.4.0/RPMS/x86_64/oracle-j2sdk1.8-1.8.0+update121-1.x86_64.rpm"
+rpm -ivh "http://archive.cloudera.com/director/redhat/7/x86_64/director/2.5.0/RPMS/x86_64/oracle-j2sdk1.8-1.8.0+update121-1.x86_64.rpm"
